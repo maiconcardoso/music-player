8390faf595ebbfb1a7fd240a04323a6305b25296
Maicon Cardoso <maiconscardoso@hotmail.com>
2023-12-29 18:32:11 -0300

    Criação da classe User

---
+++ src/main/java/model/User.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	
+	public User(int id, String name, String email, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public User() {};
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getId() {
+		return this.id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmail() {
+		return this.email;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && id == other.id && Objects.equals(name, other.name);
+	}
+	
+	
+
+}
