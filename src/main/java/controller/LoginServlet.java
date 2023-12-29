a1838ee347f5d2daf65cf3548c8e5943242ce51a
Maicon Cardoso <maiconscardoso@hotmail.com>
2023-12-29 16:14:24 -0300

    Teste de login

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -7,26 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
- */
 @WebServlet("/loginservlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+ 
+    public LoginServlet() {super();}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("Recebido " + request.getParameter("txtname"));
+		System.out.println("Recebido " + request.getParameter("txtpassword"));
 	}
 
 }
