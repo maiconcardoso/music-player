c6dfea355961bda7d1318a85037a02f8c9988c18
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-01-01 10:51:40 -0300

    Alterações de iniciar o banco de dados

--- src/main/java/dao/GenericDao.java
+++ src/main/java/dao/GenericDao.java
@@ -5,7 +5,8 @@ import java.util.List;
 public interface GenericDao {
 	
 	public void create(Object o);
-	public List<Object> read(Object o);
+	public Object read(Object o);
+	public List<Object> readAll(Object o);
 	public void update(Object o);
 	public void delete (Object o);
 
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,5 +1,65 @@
 package dao;
 
-public class UserDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+
+import model.User;
+
+public class UserDao implements GenericDao {
+	
+	private DataSource datasource;
+	
+	public UserDao (DataSource datasource) {
+		this.datasource = datasource;
+	}
+	
+	// Abrir conexão com banco de dados
+	Connection connection = null;
+
+	@Override
+	public void create(Object o) {
+			
+	}
+
+	@Override
+	public Object read(Object o) {
+		
+		try {
+			if (o instanceof User) {
+				connection = datasource.getConnection();
+				User user = (User) o;
+				String createSQL = "SELECT * FROM users WHERE email = ? AND password_ = ?";
+				PreparedStatement pst = connection.prepareStatement(createSQL);
+				pst.setString(1, user.getEmail());
+				pst.setString(2, user.getPassword());
+				pst.executeQuery();
+			}
+			connection.close();
+			return  o;
+			
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+			return null;
+		}
+		
+	}
+	
+	@Override
+	public List<Object> readAll(Object o) {
+		return null;
+	}
+
+	@Override
+	public void update(Object o) {
+		
+		
+	}
+
+	@Override
+	public void delete(Object o) {
+		
+		
+	}
 
 }
